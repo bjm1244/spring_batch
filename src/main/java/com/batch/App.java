3c3403c7fd010d3ba5fcc7a3f033208733941896
baejungmo <44220361+baejungmo@users.noreply.github.com>
2020-05-04 21:30:34 +0900

    0504 마무리 mybatis, api연동해야함.

--- src/main/java/com/batch/App.java
+++ src/main/java/com/batch/App.java
@@ -5,15 +5,28 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/*
+ * 0504
+ * xml Reader O
+ * jdbc Writer O
+ * Log O (기본 스키마로 존재)
+ * mybatis X
+ * API X
+ * 처리 시간, 재요청 X
+ * insert_schema_mysql.sql 미완성
+ * 	- 모두 실패 한 것 일 시 오류 뜸. 
+ * 	- DB 만든 후 task 실행 안하고, 끈 후 다시 실행 할 시 오류
+ */
 public class App {
 	public static void main(String[] args) {
 
 		String[] springConfig  = 
 			{	
+//				mybatis 되지않아 주석처리함.
+//				"spring/batch/config/mybatis_mapper.xml",
 				"spring/batch/config/database.xml",
 				"spring/batch/config/context.xml",
-				"spring/batch/jobs/job-report.xml",
-//				"spring/batch/config/mybatis_mapper.xml"
+				"spring/batch/jobs/job-report.xml"
 			};
 		
 		ApplicationContext context = 
