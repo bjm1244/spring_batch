16bf3e1f5b86cc5e9a2cf1215c23fc4e54045478
baejungmo <44220361+baejungmo@users.noreply.github.com>
2020-05-04 16:08:07 +0900

    xml+batch프로그램입니다.

---
+++ src/main/java/com/mkyong/RunScheduler.java
@@ -0,0 +1,33 @@
+package com.mkyong;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RunScheduler {
+	
+	@Autowired
+	JobLauncher jobLauncher;
+	@Autowired
+	Job job;
+	
+	public void run() {
+		try {
+			JobParameters jobParameters = new JobParametersBuilder()
+					  .addLong("time",System.currentTimeMillis()).toJobParameters();
+						
+			JobExecution execution = jobLauncher.run(job, jobParameters);
+			System.out.println("Exit Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+		} finally {
+			
+		}
+
+		System.out.println("Done");
+		
+	}
+}
